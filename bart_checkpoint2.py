b99eabab703c29c0a912d7d2ea6d0932310bff35
Mark Trovinger <mark.trovinger@gmail.com>
2023-04-28 16:57:53 -0400

    added code for bart for update 2

--- bart_checkpoint2.py
+++ bart_checkpoint2.py
@@ -1,16 +1,28 @@
 from datasets import load_dataset
 import transformers
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from transformers import Trainer, TrainingArguments
+from transformers import Trainer, TrainingArguments, Seq2SeqTrainingArguments, Seq2SeqTrainer
 from transformers import DataCollatorForSeq2Seq
 import evaluate
 import numpy as np
+import torch
+torch.backends.cuda.matmul.allow_tf32 = True
+
+
 
 tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
 model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
+rouge = evaluate.load("rouge")
+prefix = "summarize: "
 
-def tokenize_data(batch):
-    pass
+def preprocess_function(examples):
+    inputs = [prefix + doc for doc in examples["article"]]
+    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+
+    labels = tokenizer(text_target=examples["highlights"], max_length=512, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
@@ -28,16 +40,43 @@ def compute_metrics(eval_pred):
 
 def main():
     
-    dataset = load_dataset("cnn_dailymail")
+    dataset = load_dataset("cnn_dailymail", '3.0.0')
+
+    tokenized_dataset = dataset.map(preprocess_function, batched=True)
 
     data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
-    rouge = evaluate.load("rouge")
+    
 
-    training_args = TrainingArguments(do_train=True)
+    training_args = Seq2SeqTrainingArguments(
+        output_dir="text_summarization",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        weight_decay=0.01,
+        save_total_limit=3,
+        num_train_epochs=4,
+        predict_with_generate=True,
+        bf16=True,
+        push_to_hub=False,
+        do_eval=True, 
+        gradient_accumulation_steps=4,
+        gradient_checkpointing=True,
+        tf32=True,
+        optim="adafactor"
+    )
 
-    trainer = Trainer(model=model,
-                      training_args=training_args,
-                      eval_dataset=tokenized["te"])
+    trainer = Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["test"],
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+
 
 if __name__=='__main__':
     main()
