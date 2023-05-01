bafffda825ec1744a075f258315a5188d3e025a0
Mark Trovinger <mark.trovinger@gmail.com>
2023-05-01 18:40:04 -0400

    spacy using the full dataset, needs a couple of bugs fixed

---
+++ spacy_fulldataset.py
@@ -0,0 +1,46 @@
+import numpy as np 
+import pandas as pd
+import os 
+import evaluate
+from datasets import load_dataset
+import spacy 
+import pytextrank
+#import gensim
+
+#spacy.cli.download("en_core_web_lg")
+nlp = spacy.load('en_core_web_lg')
+nlp.add_pipe("textrank")
+metric = evaluate.load('rouge')
+
+def eval(example):
+    """
+        https://derwen.ai/docs/ptr/sample/
+    """ 
+    doc = nlp(example["article"])
+    tr = doc._.textrank
+    summary = ""
+    for sent in tr.summary(limit_phrases=10, limit_sentences=2):
+        summary += str(sent)
+    example["prediction"] = summary
+    return example
+
+def compute_metrics(example):
+    """
+    """
+    pred = example["prediction"]
+    ref = example["highlights"]
+    result = metric.compute(predictions=pred, references=ref, use_stemmer=True)
+    return result
+
+def main():
+    cnn_dailymail = load_dataset('cnn_dailymail', '3.0.0')
+    train_dataset = cnn_dailymail['train']
+    #metric = evaluate.load('rouge')
+
+    train_dataset_preds = train_dataset.map(eval, num_proc=64)
+    results = train_dataset_preds.map(compute_metrics)
+    print(results)
+
+
+if __name__=='__main__':
+    main()
