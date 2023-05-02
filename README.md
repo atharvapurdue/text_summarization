e127e7f7c75dc4ca6147001fcecfdd08f9f83c30
Mark Trovinger <mark.trovinger@gmail.com>
2023-05-02 16:38:54 -0400

    computing rouge score works, still needs some polish, also started documentation in README

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ To create the environment using conda:
 ```
 conda env create -f environment.yml
 ```
+
+# spACy, HeapQ
+To 
+
+# BART baseline
+In order to generate a baseline result for BART, the shell script `text_summarization_bart.sh` should be run, but in order to do so, the path should be changed to a local installation of `transformers` from HuggingFace, installed from source. The file `bart_baseline.out` is a text file with the shell script output from a `nohup` command.
+
+## Baseline Results:
+```
+eval_gen_len            =    83.8787
+  eval_loss               =     2.7142
+  eval_rouge1             =     44.586
+  eval_rouge2             =    21.4773
+  eval_rougeL             =    31.0223
+  eval_rougeLsum          =    41.5275
+  eval_runtime            = 1:45:18.98
+  eval_samples            =      13368
+  eval_samples_per_second =      2.116
+  eval_steps_per_second   =      0.529
+```
+
+The relevant metrics are `eval_rouge_1`, `eval_rouge_2`, and `eval_rouge_L`. The values are in line with the original paper from Facebook AI (now META AI).
--- spacy_fulldataset.py
+++ spacy_fulldataset.py
@@ -6,6 +6,7 @@ from datasets import load_dataset
 import spacy 
 import pytextrank
 #import gensim
+from rouge import Rouge
 
 #spacy.cli.download("en_core_web_lg")
 nlp = spacy.load('en_core_web_lg')
@@ -27,21 +28,29 @@ def eval(example):
 def compute_metrics(example):
     """
     """
-    pred = list(example["prediction"].split(" "))
-    #print(len(pred))
-    ref = list(example["highlights"].split(" "))
-    pred = pred[:len(ref)]
+    pred = example["prediction"]
+    ref = example["highlights"]
+    #pred = pred[:len(ref)]
     result = metric.compute(predictions=pred, references=ref)
     return result
 
+def _compute_metrics(example):
+    rouge = Rouge()
+    pred = example["prediction"]
+    ref = example["highlights"]
+
+    score = rouge.get_scores(pred, ref)
+    return score[0]
 def main():
     cnn_dailymail = load_dataset('cnn_dailymail', '3.0.0')
     train_dataset = cnn_dailymail['train']
     #metric = evaluate.load('rouge')
 
     train_dataset_preds = train_dataset.map(eval, num_proc=64)
-    results = train_dataset_preds.map(compute_metrics, num_proc=64)
-    print(results)
+    results = train_dataset_preds.map(_compute_metrics, num_proc=64)
+    rouge_results = {}
+
+    print(results["rouge-1"])
 
 
 if __name__=='__main__':
